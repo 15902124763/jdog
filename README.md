176296de7b9980d9b07e45c4f35733b4524ec2f8
yarm <15902124763@163.com>
2019-12-30 14:17:36 +0800

    #add maven package command

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jdog
 基于springboot的轻量级发布系统
+
+打包命令  
+mvn package assembly:single
---
+++ src/main/java/com/yarm/jdog/Jdog.java
@@ -0,0 +1,13 @@
+package com.yarm.jdog;
+
+/**
+ * @program: jdog
+ * @description: main入口
+ * @author: yarm.yang
+ * @create: 2019-12-30 13:59
+ */
+public class Jdog {
+    public static void main(String[] args) {
+        System.out.println("Im am ok");
+    }
+}
