bfba146fa6d3b067785eb2cf6a0a68c5bf478e77
yarm <15902124763@163.com>
2019-12-30 22:49:02 +0800

    #add git

--- src/main/java/com/yarm/jdog/command/GitClone.java
+++ src/main/java/com/yarm/jdog/command/GitClone.java
@@ -1,5 +1,9 @@
 package com.yarm.jdog.command;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program: jdog
  * @description: 获取git仓库数据
@@ -7,10 +11,14 @@ package com.yarm.jdog.command;
  * @create: 2019-12-30 14:26
  */
 public class GitClone extends AbstractCommandFactory{
-    private String GIT_URL = "git clone git@github.com:15902124763/jdog.git /home/yarm/temp/code";
+    private String GIT_URL = "git clone git@github.com:15902124763/jdog.git";
 
     public void get(){
-        this.exec(GIT_URL);
+        List<String> cmdList = new ArrayList<String>();
+        cmdList.add("cd /home/yarm/temp/code");
+        cmdList.add(GIT_URL);
+        List<String> strings = this.executeNewFlow(cmdList);
+        System.out.println(strings);
     }
 
     public void gitFetch(){
@@ -18,4 +26,38 @@ public class GitClone extends AbstractCommandFactory{
     }
 
     public void gitCheckout(){}
+
+    public void toDir(String dir){
+    }
+
+    public List<String> executeNewFlow(List<String> commands) {
+        List<String> rspList = new ArrayList<String>();
+        Runtime run = Runtime.getRuntime();
+        try {
+            Process proc = run.exec("/bin/bash", null, null);
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(proc.getOutputStream())), true);
+            for (String line : commands) {
+                out.println(line);
+            }
+            // out.println("cd /home/test");
+            // out.println("pwd");
+            // out.println("rm -fr /home/proxy.log");
+            out.println("exit");// 这个命令必须执行，否则in流不结束。
+            String rspLine = "";
+            while ((rspLine = in.readLine()) != null) {
+                System.out.println(rspLine);
+                rspList.add(rspLine);
+            }
+            proc.waitFor();
+            in.close();
+            out.close();
+            proc.destroy();
+        } catch (IOException eio) {
+            eio.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return rspList;
+    }
 }
